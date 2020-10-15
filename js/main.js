18d93f7f29881a74ad923a4684af423544bb25af
summer4requiem <summer.requiem96@gmail.com>
2020-10-15 14:01:38 +0300

    changed issue

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 "use strict";
-(function () {
+(() => {
   const adForm = document.querySelector(`.ad-form`);
   const mainPin = document.querySelector(`.map__pin--main`);
   const fieldSets = document.querySelectorAll(`fieldset`);
-  const addressInput = document.querySelector(`#address`);
+
   const LEFT_MOUSE_CLICK_KEY_CODE = 1;
   const ENTER_CODE = `Enter`;
   const map = document.querySelector(`.map`);
   adForm.classList.add(`ad-form--disabled`);
-
 
   const removeDisablePageAttributes = () => {
     fieldSets.forEach((field) => {
@@ -16,9 +15,7 @@
       map.classList.remove(`map--faded`);
       adForm.classList.remove(`ad-form--disabled`);
     });
-    addressInput.value = (570 + 22) + ` x ` + (375 + 10) + ` y`;
     render.pins();
-    mainPin.removeEventListener(`click`, removeDisablePageAttributes);
     mainPin.removeEventListener(`mousedown`, onMouseDownActivatePage);
     mainPin.removeEventListener(`keydown`, onKeyDownActivatePage);
   };
